7c78e06a67df479cf933ceaf26ff114f63a27836
rpikul <radoslaw.pikul@ringieraxelspringer.pl>
2025-11-15 14:47:52 +0100

    Absstract class for all encoder/decoders

---
+++ conversions_service/conversions_service/services/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractEncoder(ABC):
+
+    @classmethod
+    def get_instance(cls):
+        if not hasattr(cls, "_instance"):
+            cls._instance = cls.__new__(cls)
+        return cls._instance
+
+    @abstractmethod
+    def encode(self, data: str, key: str, **kwargs) -> str:
+        raise NotImplementedError("Subclasses must implement this method")
+
+    @abstractmethod
+    def decode(self, data: str, key: str, **kwargs) -> str:
+        raise NotImplementedError("Subclasses must implement this method")
