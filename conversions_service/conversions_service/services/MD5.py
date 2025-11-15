0d16c619d9897d866aecf7785893f74c3f1e3c06
Marcin <marcins137@wp.pl>
2025-11-15 15:35:46 +0100

    Add implementation for MD5, sha256 and sha512

---
+++ conversions_service/conversions_service/services/MD5.py
@@ -0,0 +1,14 @@
+from base import AbstractEncoder
+import hashlib  
+
+class MD5(AbstractEncoder):
+
+    _instance = None
+
+    def encode(self, data: str, key: str = None, **kwargs) -> str:
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(data.encode('utf-8'))
+        return sha256_hash.hexdigest()
+
+    def decode(self, data: str, key: str = None, **kwargs) -> str:
+        raise NotImplementedError("SHA-256 is a one-way hash function and cannot be decoded")
