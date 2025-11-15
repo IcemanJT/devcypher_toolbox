cbc23cdc237f718dd05976cd610527d304f36cf4
Marcin <marcins137@wp.pl>
2025-11-15 15:41:52 +0100

    Update MD5

--- conversions_service/conversions_service/services/MD5.py
+++ conversions_service/conversions_service/services/MD5.py
@@ -6,9 +6,8 @@ class MD5(AbstractEncoder):
     _instance = None
 
     def encode(self, data: str, key: str = None, **kwargs) -> str:
-        sha256_hash = hashlib.sha256()
-        sha256_hash.update(data.encode('utf-8'))
-        return sha256_hash.hexdigest()
+        md5 = hashlib.md5(data.encode())
+        return md5.hexdigest()
 
     def decode(self, data: str, key: str = None, **kwargs) -> str:
-        raise NotImplementedError("SHA-256 is a one-way hash function and cannot be decoded")
+        raise NotImplementedError("MD5 is a one-way hash function and cannot be decoded")
