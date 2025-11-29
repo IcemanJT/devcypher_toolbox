4b1c12e17a5dbf10a75236f5be5cdd67bc3630c4
rpikul <radoslaw.pikul@ringieraxelspringer.pl>
2025-11-29 14:15:32 +0100

    Fix whatever happened with the tests

---
+++ conversions_service/conversions_service/services/md5_encoder.py
@@ -0,0 +1,13 @@
+from .base import AbstractEncoder
+import hashlib
+
+class MD5Encoder(AbstractEncoder):
+    name: str = "md5"
+    is_symmetric: bool = False
+
+    def encode(self, data: str, key: str, **kwargs) -> str:
+        md5_hash = hashlib.md5(data.encode('utf-8'))
+        return md5_hash.hexdigest()
+
+    def decode(self, data: str, key: str, **kwargs) -> str:
+        raise NotImplementedError("MD5 is a one-way hash function and cannot be decoded.")
